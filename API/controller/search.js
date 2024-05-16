33e6a896cf87c0962d3b8fde14d306e1fca1dfed
Đặng Tiến Dũng <dangtiendung1201@gmail.com>
2024-05-16 19:52:24 +0700

    chore: Change query of search posts

--- API/controller/search.js
+++ API/controller/search.js
@@ -1,13 +1,14 @@
 import esClient from "../../elasticSearch.js";
 
 export const searchPost = async (req, res) => {
-  const { query } = req.body;
+  // Get query from params "search?query=..."
+  const query = req.query.query;
 
   try {
     const { body } = await esClient.search({
       index: "posts",
       filter_path: ["hits.hits._source", "aggregations.*"],
-      sort: ["_score:desc, createdAt:desc"],
+      sort: ["_score:desc"],
       size: 5,
       body: {
         query: {
@@ -15,7 +16,7 @@ export const searchPost = async (req, res) => {
         },
       },
     });
-    
+
     if (!body.hits) {
       res.status(404).send("No results found");
       return;
