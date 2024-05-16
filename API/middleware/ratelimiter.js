ba27cfbe5456a9bdef7abe0e28fdd54234b00844
Đặng Tiến Dũng <dangtiendung1201@gmail.com>
2024-05-16 11:38:13 +0700

    feat: Use Redis rate limit for register

--- API/middleware/ratelimiter.js
+++ API/middleware/ratelimiter.js
@@ -2,22 +2,22 @@ import { createClient } from 'redis';
 import { RateLimiterRedis } from 'rate-limiter-flexible';
 
 const redisClient = createClient({
-  url:process.env.REDIS_ENDPOINT_URI,
+  url: process.env.REDIS_ENDPOINT_URI,
 });
 
 redisClient.on('error', (err) => console.log('Redis Client Error', err));
 
 await redisClient.connect();
 
-const opts= {
+const opts = {
   storeClient: redisClient,
   keyPrefix: 'middleware',
-  points: 50, // requests
-  duration: 10, // per second
+  points: 5, // requests
+  duration: 60, // per second
 };
 const rateLimiter = new RateLimiterRedis(opts);
 
-const rateLimiterMiddleware = (req, res, next) => {
+export const rateLimiterMiddleware = (req, res, next) => {
   console.log(`Request: ${req.ip}`)
   rateLimiter.consume(req.connection.remoteAddress)
     .then(() => {
@@ -30,5 +30,3 @@ const rateLimiterMiddleware = (req, res, next) => {
       res.status(429).send('Too Many Requests');
     });
 };
-
-export default rateLimiterMiddleware;
